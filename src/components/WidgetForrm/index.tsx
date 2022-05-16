3735c5eab423ac76ef71545b34027f0fc9d2a8cd
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2022-05-16 09:50:59 -0300

    Adicionando o botão de screenshot e o botão enviar

--- src/components/WidgetForrm/index.tsx
+++ src/components/WidgetForrm/index.tsx
@@ -6,6 +6,7 @@ import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -42,14 +43,10 @@ export function WidgetForm() {
 
     return (
         <>
-            <header>
-                <span className="text-xl leading-6">Deixe seu Feedback</span>
-                <CloseButton/>
-            </header>
 
             <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
                 { !feedbackType ? (
-                    <FeedbackTypeStep onFeedbackRestartRequested={setFeedbackType} />
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
                 ) : (
                     <FeedbackContentStep 
                         feedbackType={feedbackType} 
