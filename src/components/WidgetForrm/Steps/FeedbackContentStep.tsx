a5b0d5b98c2ecdf38cc7dad28aa276ac7791ba20
JhordhanCarlos <36717504+JhordhanCarlos@users.noreply.github.com>
2022-05-18 09:30:53 -0300

    Finalizando o front end

--- src/components/WidgetForrm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForrm/Steps/FeedbackContentStep.tsx
@@ -1,63 +1,58 @@
-import { ArrowLeft, Camera } from 'phosphor-react'
-import { FormEvent, useState } from 'react';
-import { feedbackTypes, FeedbackType } from "..";
-import { CloseButton } from "../../CloseButton";
-import { ScreenshotButton } from '../ScreenshotButton';
 
-interface FeedbackContentStepProps{
-    feedbackType : FeedbackType;
+import { ArrowLeft, Camera } from "phosphor-react";
+import { feedbackTypes, FeedbackType } from "../index";
+import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
+import { FormEvent, useState } from "react";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 
-export function FeedbackContentStep ({feedbackType, onFeedbackRestartRequested} : FeedbackContentStepProps){
-    const [comment, setComment] = useState('')
-    const [screenshot, setScreenshot] = useState<string | null>('')
-    const feedbackTypeInfo = feedbackTypes[feedbackType]
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
+    
+    const [screenShot, setScreenShot] = useState<string |null>(null);
+    const [comment, setComment] = useState('');
+    
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
 
-    function handleSubmitFeeback(event: FormEvent){
+    function handleSubmitFeedback(event: FormEvent){
         event.preventDefault();
-        const feedback = {comment, screenshot}
-        console.log(comment)
+        onFeedbackSent();
+        
     }
 
-    return(
+    return (
         <>
             <header>
-                <button 
-                    type="button" 
-                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" 
-                    onClick={onFeedbackRestartRequested}
-                >
-                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" onClick={onFeedbackRestartRequested}>
+                    <ArrowLeft weight="bold" className="w-4 h4" />
                 </button>
+
                 <span className="text-xl leading-6">
-                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"/>
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
                     {feedbackTypeInfo.title}
                 </span>
 
-                <CloseButton/>
+                <CloseButton />
             </header>
 
-            <form onSubmit={handleSubmitFeeback} className="my-4 w-full">
-                <textarea 
-                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-                    placeholder="Conte com detalhes o que está acontecendo..."
-                    onChange={(e) => setComment(e.target.value)}
-                />
-                <footer className="flex gap-2 mt-2">
-                    <ScreenshotButton 
-                        screenshot={screenshot}
-                        onScreenshotTook={setScreenshot}
+            <div className="flex py-8 gap-2 w-full">
+                <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+                    <textarea
+                        onChange={event => setComment(event.target.value)}
+                        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                        placeholder="Conte com detalhes o que está acontecendo..."
                     />
-                    <button
-                        type="submit"
-                        disabled={comment.length === 0 ? true : false } 
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
-                    >
-                        Enviar
-                    </button>
-                </footer>
-            </form>
+                    <footer className="flex gap-2 mt-2">
+                        <ScreenshotButton screenshot={screenShot} onScreenshotTook={setScreenShot}></ScreenshotButton>
+
+                        <button type="submit" disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">Enviar feedback</button>
+                    </footer>
+                </form>
+            </div>
         </>
-    );
+    )
 }
--- src/components/WidgetForrm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForrm/Steps/FeedbackSuccessStep.tsx
@@ -1,5 +1,29 @@
-export function FeedbackSuccesstStep (){
+import { CloseButton } from "../../CloseButton";
+import CheckImage from '../../../assets/check.svg';
+
+interface FeedbackTypeStepProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackTypeStepProps) {
     return (
-        <></>
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+            <img src={CheckImage} />               
+
+                <span className="text-xl mt-2">Agradecemos o feedback</span>
+                <button
+                    type="button"
+                    onClick={onFeedbackRestartRequested}
+                    className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 "
+                >
+                    Quero enviar outro
+                </button>
+            </div>
+        </>
     );
 }
